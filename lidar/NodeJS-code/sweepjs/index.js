72da89731ed4a3a1cc5f19156953a269270f40d9
Ryan Diaz <tdiaz@highpoint.edu>
2018-11-01 20:51:02 -0400

    updated index.js

--- lidar/NodeJS-code/sweepjs/index.js
+++ lidar/NodeJS-code/sweepjs/index.js
@@ -6,31 +6,32 @@ var util = require('util')
 
 
 if (require.main === module) {
-  console.log('self-testing module');
+	console.log('self-testing module');
 
-  var portName = process.argv[2];
-  var sweep = new sweepjs.Sweep(portName);
+	var portName = process.argv[2];
+	var sweep = new sweepjs.Sweep(portName);
 
-  var speed = sweep.getMotorSpeed();
-  var rate = sweep.getSampleRate();
+	var speed = sweep.getMotorSpeed();
+	var rate = sweep.getSampleRate();
 
-  console.log(util.format('Motor speed: %d Hz', speed));
-  console.log(util.format('Sample rate: %d Hz', rate));
 
-  console.log('Starting data acquisition as soon as motor is ready...');
-  sweep.startScanning();
+	console.log(util.format('Motor speed: %d Hz', speed));
+	console.log(util.format('Sample rate: %d Hz', rate));
 
-  sweep.scan(function (err, samples) {
-    if (err) {
-      return console.log(err);
-    }
+	console.log('Starting data acquisition as soon as motor is ready...');
+	sweep.startScanning();
 
-    samples.forEach(function (sample) {
-      var fmt = util.format('angle: %d distance %d signal strength: %d',
-        sample.angle, sample.distance, sample.signal);
-      console.log(fmt);
-    });
-  });
+	sweep.scan(function (err, samples) {
+		if (err) {
+  			return console.log(err);
+		}
+
+		samples.forEach(function (sample) {
+  			var fmt = util.format('angle: %d distance %d signal strength: %d',
+    		sample.angle, sample.distance, sample.signal);
+  			console.log(fmt);
+		});
+	});
 }
 
 module.exports = sweepjs;
